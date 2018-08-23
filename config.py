331669a186d3f3d1de30607149517b7ae334f7d0
Fred Z <freezed@users.noreply.github.com>
2018-08-24 00:14:35 +0200

    Adds a configuration file #9

---
+++ config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: freezed <freezed@users.noreply.github.com> 2018-08-23
+Version: 0.1
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+This file is part of [ocp7](http://github.com/freezed/ocp7/) project.
+
+"""
+PORT = 1664
+HOST = '192.168.1.70'
+APP = {
+    'NAME': 'GrandPy Bot, le papy-robot',
+    'SRC': 'http://github.com/freezed/ocp7/',
+    'DEBUG': True,
+}
--- flasklocal/views.py
+++ flasklocal/views.py
@@ -12,6 +12,7 @@ This file is part of [ocp7](http://github.com/freezed/ocp7/) project.
 from flask import Flask, request
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -21,11 +22,16 @@ def index():
             response = request.form['text']
 
     return """
+    {name}<br />
+    basic Flask application
     <form method="post">
         <input type="text" name="text" maxlength=25 \>
         <input type="submit" name="submit" value="Send" \>
     </form>
-    Posted data : {}""".format(response)
+    Posted data : {response}""".format(
+        name    =app.config['APP']['NAME'],
+        response=response,
+    )
 
 if __name__ == "__main__":
     app.run()
