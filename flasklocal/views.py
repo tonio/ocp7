d2cac646a543883ee3860f9bbb37f59508598d4b
Fred Z <freezed@users.noreply.github.com>
2018-08-27 20:45:50 +0200

    Gives address to generate map #4

--- flasklocal/views.py
+++ flasklocal/views.py
@@ -11,7 +11,7 @@ This file is part of [ocp7](http://github.com/freezed/ocp7/) project.
 """
 import pprint
 from flask import Flask, request, render_template
-from apicall import goo_geocode, goo_static
+from apicall import goo_geocode, goo_static, filter_api_response
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,22 +21,29 @@ app.config.from_object('config')
 def index():
     """ Index View """
 
+    # Default response
+    filtered_data_formated = "… empty response …"
+    map_url = "https://via.placeholder.com/600x300?text=no+map"
+
     # Catch posted data from form
     if "submit" in request.form:
-        address = str(request.form['query'])
-        api_response = pprint.pformat(goo_geocode(address))
+        query_string = str(request.form['query'])
 
-    else:
-        # Default response
-        api_response = "… empty response …"
+        # Request & filter data
+        filtered_data = filter_api_response(goo_geocode(query_string))
+
+        # Get map URL for address
+        map_url = goo_static(filtered_data['formatted_address'])
+
+        filtered_data_formated = pprint.pformat(filtered_data)
 
     # Redern view with vars
     return render_template(
         "index.html",
         name=app.config['APP']['NAME'],
         url=app.config['APP']['SRC'],
-        raw_response=api_response,
-        map_url=goo_static(),
+        raw_response=filtered_data_formated,
+        map_url=map_url,
     )
 
 
