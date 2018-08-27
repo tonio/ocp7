8b2407e110403706430a93a47476291b866faeee
Fred Z <freezed@users.noreply.github.com>
2018-08-27 20:23:40 +0200

    Filters data returned by geocoding API #22

--- apicall.py
+++ apicall.py
@@ -57,5 +57,35 @@ def goo_static(center="7 Cité Paradis, 75010 Paris, France", size=(600,300)):
     return response.url
 
 
+def filter_api_response(response=goo_geocode()):
+    """
+    Filter API's response to feed maps generation & MediaWiki request
+
+    """
+    # Dict aliases for smaller lines
+    alias_ac = response['results'][0]['address_components']
+    alias_go = response['results'][0]
+    alias_vp = response['results'][0]['geometry']['viewport']
+
+    # No data if request ĥas less or more 1 result
+    if len(response['results']) != 1:
+        filtered_data['unclear'] = True
+
+    else:
+        filtered_data = {'truncated_address': [
+            {alias_ac[1]['types'][0]: alias_ac[1]['long_name']},
+            {alias_ac[2]['types'][0]: alias_ac[2]['long_name']},
+            {alias_ac[3]['types'][0]: alias_ac[3]['long_name']},
+            {alias_ac[4]['types'][0]: alias_ac[4]['long_name']}
+        ]}
+
+        filtered_data['formatted_address'] = alias_go['formatted_address']
+        filtered_data['location'] = alias_go['geometry']['location']
+        filtered_data['viewport_ne'] = alias_vp['northeast']
+        filtered_data['viewport_sw'] = alias_vp['southwest']
+
+    return filtered_data
+
+
 if __name__ == "__main__":
-    goo_static()
+    filter_api_response()
